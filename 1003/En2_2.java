20bdb2caf0ccd5a58dc07755243353d712439de9
s18008 <s18008@std.it-college.ac.jp>
2018-10-03 16:21:23 +0900

    10月3日の課題

---
+++ 1003/En2_2.java
@@ -0,0 +1,13 @@
+public class En2_2{
+public static void main(String[] args){
+for (int i = 1; i <= 100; i++){
+    String str = String.valueOf(i);
+    if (i % 3 == 0 ||str.contains("3") ){
+    System.out.println("THREE");
+    }else{
+     System.out.println(i);
+    }
+}
+}
+}
+
