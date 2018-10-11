9086c9acbfe2077f56cc246eea2277c265a5a097
s18008 <s18008@std.it-college.ac.jp>
2018-10-11 09:50:04 +0900

    10月10日課題

---
+++ 1010/Main.java
@@ -0,0 +1,16 @@
+//package jp.co.example.java15;
+
+import java.util.*;
+//import itcollage.*;
+
+public class Main {
+    public static void main(String[] args){
+       printNumbers(PrimeNumberCalcultor.calcTo(100));
+    }
+
+    private static void printNumbers(List<Integer> numbers){
+     for (int number: numbers){
+         System.out.println(number);
+    }
+        }
+      }
